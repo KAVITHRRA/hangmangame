879db5a1cc0a2fcd07fcc1b3a18a9ed34e54d938
KAVITHRRA PADMA RAJ <119802464+KAVITHRRA@users.noreply.github.com>
2023-05-01 23:37:49 +0800

    code
    code

---
+++ Main.py
@@ -0,0 +1,126 @@
+import random
+
+word_list = ["ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote"]
+
+hangman_pics = [
+    """
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========
+    """
+]
+
+
+
+def get_word():
+    return random.choice(word_list)
+
+
+
+def display_game(word, guesses, incorrect_guesses):
+    
+    print(hangman_pics[len(incorrect_guesses)])
+
+    
+    displayed_word = "".join([letter if letter in guesses else "_" for letter in word])
+    print(displayed_word)
+
+    
+    remaining_guesses = max_guesses - len(incorrect_guesses)
+    print("Remaining guesses:", remaining_guesses)
+
+
+
+word = get_word()
+max_guesses = 6
+guesses = set()
+incorrect_guesses = set()
+
+
+while True:
+    display_game(word, guesses, incorrect_guesses)
+
+    
+    if all(letter in guesses for letter in word):
+        print("You win!")
+        break
+
+    
+    if len(incorrect_guesses) == max_guesses:
+        print("You lose! The word was", word)
+        break
+
+    
+    guess = input("Guess a letter: ").lower()
+
+    
+    if len(guess) != 1 or not guess.isalpha() or guess in guesses.union(incorrect_guesses):
+        print("Invalid guess. Please guess a single letter that you haven't guessed before.")
+        continue
+
+    
+    if guess in word:
+        guesses.add(guess)
+        print("Correct!")
+    else:
+        incorrect_guesses.add(guess)
+        print("Incorrect!")
+        
